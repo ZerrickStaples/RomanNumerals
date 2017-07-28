4a3fcf91e950e2cbf6ac41cc653a45b9ede9d0f9
Zerrick Staples <zerrick.staples@gmail.com>
2017-07-28 10:58:22 -0400

    Created, failed and successfully passed tests for for L and C.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
 export const RomanNumeral = (inputNumber) => {
 
+    /* var obj = {
+        1: 'I',
+        2: 'II',
+        3: 'III',
+        4: 'IV',
+        5: 'V',
+        10: 'X',
+        50: 'L',
+        100: 'C',
+    }
+
+    for (var key in obj) {
+        if (i >= 1, i <= 3, i++) {
+            return key;
+        }
+    }
+    */
+
     if (inputNumber === 1) {
         return 'I';
+    } else if (inputNumber === 2) {
+        return 'II';
+    } else if (inputNumber === 3) {
+        return 'III';
+    } else if (inputNumber === 4) {
+        return 'IV';
     } else if (inputNumber === 5) {
         return 'V';
     } else if (inputNumber === 10) {
         return 'X';
+    } else if (inputNumber === 50) {
+        return 'L';
+    } else if (inputNumber === 100) {
+        return 'C';
     }
 }
--- test/index.spec.js
+++ test/index.spec.js
@@ -13,11 +13,35 @@ describe("RomanNumeral Kata", () => {
         expect(RomanNumeral(1)).to.equal('I');
     })
 
-    it("Return V When 5 Is Input", () => {
+    it("Return II When 2 is input", () => {
+        expect(RomanNumeral(2)).to.equal('II');
+    })
+
+    it("Return III When 3 is input", () => {
+        expect(RomanNumeral(3)).to.equal('III');
+    })
+
+    it("Return IV when 4 is input", () => {
+        expect(RomanNumeral(4)).to.equal('IV');
+    })
+
+    it("Return V when 5 is input", () => {
         expect(RomanNumeral(5)).to.equal('V');
     })
 
-    it("Return X When 10 Is Input", () => {
+    it("Return X when 10 is input", () => {
         expect(RomanNumeral(10)).to.equal('X');
     })
+
+    it("Return L When 50 is input", () => {
+        expect(RomanNumeral(50)).to.equal('L');
+    })
+
+    it("Return C when 100 is input", () => {
+        expect(RomanNumeral(100)).to.equal('C');
+    })
+
+    it("repeat letters only 3 times"), () => {
+        expect(RomanNumeral()).to.equal();
+    }
 })
