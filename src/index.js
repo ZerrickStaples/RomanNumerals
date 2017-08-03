69542bd8a9e313b72703f94e865ddf3a36cf335c
Zerrick Staples <zerrick.staples@gmail.com>
2017-08-03 12:17:14 -0400

    First commit. Set up testing environment.

--- src/index.js
+++ src/index.js
@@ -1,32 +1,3 @@
-export const RomanNumeral = (inputNumber) => {
+export const ArabicNumbers = (inputNumeral) => {
 
-    //Created array of objects. Key: Value relationship
-    var numeral = [
-        { input: 1000, output: "M" },
-        { input: 900, output: "DM" },
-        { input: 500, output: "D" },
-        { input: 400, output: "CD" },
-        { input: 100, output: "C" },
-        { input: 50, output: "L" },
-        { input: 40, output: "XL" },
-        { input: 10, output: "X" },
-        { input: 5, output: "V" },
-        { input: 4, output: "IV" },
-        { input: 1, output: "I" }
-    ]
-
-    //Setting outputNumber to blank string to define.
-    var outputNumeral = "";
-    //For each object in the numeral array.
-    numeral.forEach(function(object) {
-        //While the inputNumber is >= the input
-        while (inputNumber >= object.input) {
-            /*The outputNumber is adding the output 
-            roman numeral to itself.*/
-            outputNumeral += object.output;
-            //The inputNumber subtracts the object input from itself.
-            inputNumber -= object.input;
-        }
-    });
-    return outputNumeral;
 }
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,50 +1,9 @@
 import { expect } from 'chai';
 import Mocha from 'mocha';
-import { RomanNumeral } from '../src/index';
+import { ArabicNumbers } from '../src/index';
 
 describe("Canary test", () => {
     it("should return true", () => {
         expect(true).to.equal(true);
-    })
-})
-
-describe("RomanNumeral Kata", () => {
-    it("Return I When 1 is input", () => {
-        expect(RomanNumeral(1)).to.equal('I');
-    })
-
-    it("Return IV when 4 is input", () => {
-        expect(RomanNumeral(4)).to.equal('IV');
-    })
-
-    it("Return V when 5 is input", () => {
-        expect(RomanNumeral(5)).to.equal('V');
-    })
-
-    it("Return X when 10 is input", () => {
-        expect(RomanNumeral(10)).to.equal('X');
-    })
-
-    it("Return XL when 40 is input", () => {
-        expect(RomanNumeral(40)).to.equal('XL');
-    })
-
-    it("Return L When 50 is input", () => {
-        expect(RomanNumeral(50)).to.equal('L');
-    })
-
-    it("Return C when 100 is input", () => {
-        expect(RomanNumeral(100)).to.equal('C');
-    })
-
-    it("Return D when 500 is input", () => {
-        expect(RomanNumeral(500)).to.equal('D');
-    })
-    it("Return M when 1000 is input", () => {
-        expect(RomanNumeral(1000)).to.equal('M');
-    })
-
-    it("Return CCCXXXIV when 334 is input", () => {
-        expect(RomanNumeral(334)).to.equal('CCCXXXIV');
     })
 })
